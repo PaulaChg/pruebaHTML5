46ad626081ffaf3033cb27697d912c63c12f1630
PaulaChg <uo195202@uniovi.es>
2013-08-09 14:29:49 +0200

    Actualizaciones
    Mejorando el aspecto de la pagina utilizando CSS.

--- trabajo/codigoFuncion.js
+++ trabajo/codigoFuncion.js
@@ -2,3 +2,25 @@
    element.innerHTML = "¿Qué tal?";
    element.style.fontSize = "5em";
   }
+  
+  
+  
+  function validaform(){
+    if($("nombre").val()==" "){
+	    alert("Por favor, introduce tu nombre y apellidos");
+		$("nombre").focus();
+		return false;
+	}
+	if($("correo").val()==""){
+	   alert("Por favor, introduce tu correo electronico");
+	   $("correo").focus();
+	   return false;
+	}
+	if($("NIF").val()==""){
+	   alert("Por favor, introduce tu DNI junto a la letra correspondiente");
+	   $("NIF").focus();
+	   return false;
+	}
+	
+	return true;
+}
